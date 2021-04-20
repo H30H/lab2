fd69e76f89a5c13ef435c3699a2dcc07b2291b96
Grisha <gkurennyy@mail.ru>
2021-04-20 09:07:49 +0300

    Final push

--- cmake-build-debug/CMakeFiles/lab2.dir/cmake_clean.cmake
+++ cmake-build-debug/CMakeFiles/lab2.dir/cmake_clean.cmake
@@ -7,9 +7,8 @@ file(REMOVE_RECURSE
   "CMakeFiles/lab2.dir/myListSequence.cpp.o"
   "CMakeFiles/lab2.dir/myPolynomial.cpp.o"
   "CMakeFiles/lab2.dir/tests.cpp.o"
-  "lab2.exe"
+  "lab2"
   "lab2.pdb"
-  "liblab2.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
